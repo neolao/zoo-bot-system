c12f599b4197359107620c804108719df7ac96bd
neolao <contact@neolao.com>
2014-05-14 02:31:56 +0200

    Update

--- Puppetfile
+++ Puppetfile
@@ -2,5 +2,5 @@ forge "http://forge.puppetlabs.com"
 
 mod "puppetlabs/ntp"
 mod "puppetlabs/apt"
-mod "camptocamp/puppet-archive"
+mod "camptocamp/archive", :git => "https://github.com/camptocamp/puppet-archive.git"
 mod "saz/locales", :git => "https://github.com/saz/puppet-locales.git"
--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -3,6 +3,9 @@ class nodejs {
     archive { 'node-v0.11.12':
         ensure => present,
         url    => 'http://nodejs.org/dist/v0.11.12/node-v0.11.12-linux-arm-pi.tar.gz',
+        follow_redirects => true
         target => '/opt',
+        checksum => false,
+        src_target => '/tmp'
     }
 }
