849c38088c5df4cc1dfaf519887fa977dee17f9b
neolao <contact@neolao.com>
2014-05-14 02:34:03 +0200

    typo

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -3,7 +3,7 @@ class nodejs {
     archive { 'node-v0.11.12':
         ensure => present,
         url    => 'http://nodejs.org/dist/v0.11.12/node-v0.11.12-linux-arm-pi.tar.gz',
-        follow_redirects => true
+        follow_redirects => true,
         target => '/opt',
         checksum => false,
         src_target => '/tmp'
