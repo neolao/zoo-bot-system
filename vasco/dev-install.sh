b3e06d92bc6873097ce7f9a6ac2a023834259aad
neolao <contact@neolao.com>
2014-12-13 15:44:18 +0100

    Add TMUX to the dev tools

--- vasco/dev-install.sh
+++ vasco/dev-install.sh
@@ -4,7 +4,7 @@
 cd ~/
 
 # Install packages
-sudo apt-get install -y git zsh unzip ctags vim gcc
+sudo apt-get install -y git zsh unzip ctags vim tmux gcc
 
 # Install ZSH
 wget http://doc.neolao.com/zsh.zip
@@ -17,8 +17,12 @@ wget http://doc.neolao.com/vim.zip
 unzip vim.zip
 rm vim.zip
 
+# Install TMUX
+wget http://doc.neolao.com/.tmux.conf
+
 # Install WiringPi
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 ./build
+
 
