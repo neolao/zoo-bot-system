a9e72943fbe09b0446daea9034ec74cd87b33b98
neolao <contact@neolao.com>
2014-12-19 01:08:40 +0100

    Update Buzzer

--- vasco/sandbox/buzzer.c
+++ vasco/sandbox/buzzer.c
@@ -6,14 +6,25 @@
 
 int main(int argc, char **argv)
 {
-        int o = 0;
-        for (int k = 0; argv[1][k] != '\0'; ++k) {
-            o *= 10; o += argv[1][k] - '0';
-        }
+    if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
+        printf("setup wiringPi failed !");
+        return 1;
+    }
 
-        printf("Play %d ...\n", o);
-        softToneWrite(BuzPin, o);
-        delay(1000);
+    if(softToneCreate(BuzPin) == -1){
+        printf("setup softTone failed !");
+        return 1;
+    }
 
-        return 0;
+
+    int o = 0;
+    for (int k = 0; argv[1][k] != '\0'; ++k) {
+        o *= 10; o += argv[1][k] - '0';
+    }
+
+    printf("Play %d ...\n", o);
+    softToneWrite(BuzPin, o);
+    delay(1000);
+
+    return 0;
 }
